2a3871b9d4148e80f53d5e0eccf7352e32f75984
Idun9000 <tildeidunsloth@live.dk>
2024-11-27 14:57:57 +0100

    updating scripts

--- .ipynb_checkpoints/install_packages-checkpoint.sh
+++ .ipynb_checkpoints/install_packages-checkpoint.sh
@@ -1,3 +1,8 @@
 . /work/NLP_exam/NLP_gender_bias/virt_env/bin/activate
-python -m pip install gutenberg
+python -m pip install nltk
+python -m pip install re
+python -m pip install json
+python -m pip install numpy
+python -m pip install gensim
+python -m pip install matplotlib
 echo Done!
